98be82137dd49bbf60c29f399aacf40a5c72650d
Pierre <56385426+pierrem43@users.noreply.github.com>
2021-09-02 21:28:00 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+  if(!empty($_POST["send"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    $toEmail = "VotreAdresse@gmail.com";
+    $mailHeaders = "From: " . $name . "<". $email .">\r\n";
+    if(mail($toEmail, $subject, $message, $mailHeaders)) {
+      $mail_msg = "Vos informations de contact ont été reçues avec succés.";
+      $type_mail_msg = "success";
+    }else{
+      $mail_msg = "Erreur lors de l'envoi de l'e-mail.";
+      $type_mail_msg = "error";
+    }
+  }
+?>
